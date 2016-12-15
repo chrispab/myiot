c69edc9906d10c18233bf879923af72d070fbd48
chrispab <cbattisson@gmail.com>
2016-12-15 12:37:48 +0000

    AJAX for graph updates 1

--- app/Http/Controllers/SomeController.php
+++ app/Http/Controllers/SomeController.php
@@ -9,12 +9,16 @@ use App\Http\Requests;
 class SomeController extends Controller
 {
     //
-    public function testfunction(Illuminate\Http\Request $request)
+    public function testfunction( Request $request)
     {
         if ($request->isMethod('post')){
             return response()->json(['response' => 'This is post method']);
         }
-        //return Response::json( $response );
-        return Response::json(['response' => 'This is get method']);
+        //\Response::json( ['response' => 'This is get method'] );
+        //echo "about to return data from get";
+        //die();
+        return response()->json(['response' => 'This is get method YAY']);
+        //return (['response' => 'This is get method']);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,68 @@
 
 use Illuminate\Support\Facades\DB;
 
-
 Auth::routes();
 
 
 Route::get('/', function () {
     return view('dashboard');
-
 })->middleware('auth');
 
 
 //Route::get('graph/{hours}', 'GraphController@home')->middleware('auth');
 //Route::get('graph2/{hours}', 'Graph2Controller@home')->middleware('auth');
 Route::get('graph/{zone}/{hours}', 'GraphZoneController@home')->middleware('auth');
+Route::get('ajaxgraph/{zone}/{hours}', 'GraphZoneController@ajaxgraph')->middleware('auth');
+
+Route::get('getgraphdata/{zone}/{hours}', 'GraphZoneController@getgraphdata')->middleware('auth');
+
 
 Route::get('/dashboard', 'DashboardController@home')->middleware('auth');
 
 Route::get('/home', 'HomeController@index')->middleware('auth');
 
 Route::get('/test', 'SomeController@testfunction');
-//Route::post('/test', 'SomeController@testfunction');
+Route::post('/test', 'SomeController@testfunction');
+
 Route::get('/ajax', 'AjaxController@ajax');
+
+Route::get('/paintings', function () {
+    //$data = '{"id": 1, "title": "Mona Lisa", "painter_id": 2, "created_at": "2014-01-24 19:49:55" }';
+$data='{
+    "title": "A cool blog post",
+    "clicks": 4000,
+    "children": null,
+    "published": true,
+    "comments": [
+        {
+            "author": "Mister X",
+            "message": "A really cool posting"
+        },
+        {
+            "author": "Misrer Y",
+            "message": "Its me again!"
+        }
+    ]
+}';
+// '{
+    //   "id": 1,
+    //     "title": "Mona Lisa",
+    //     "body": "The Mona Lisa is a half-length portrait of a",
+    //     "painter_id": 2,
+    //     "created_at": "2014-01-24 19:49:55",
+    //     "updated_at": "2014-01-24 19:49:55"
+    // },
+    // {
+    //     "id": 2,
+    //     "title": "Last Supper",
+    //     "body": "The Last Supper is a late 15th-century mural",
+    //     "painter_id": 2,
+    //     "created_at": "2014-01-24 19:55:46",
+    //     "updated_at": "2014-01-24 19:55:46"
+    // }';
+
+    //return $paintings;
+    var_dump(json_decode($data));
+    var_dump(json_decode($data, true));
+
+});
