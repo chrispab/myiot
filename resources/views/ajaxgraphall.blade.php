eac69530b133d4920cb62f0bf88d9f7c883f65e5
chrispab <cbattisson@gmail.com>
2016-12-20 18:39:47 +0000

    chart js redo1

--- resources/views/ajaxgraphall.blade.php
+++ resources/views/ajaxgraphall.blade.php
@@ -11,9 +11,6 @@ $.ajaxSetup({
 </script>
 <script src="/js/loadingoverlay.min.js"></script>
 <script src="/js/mychart.js"></script>
-
-
-
 <title id="title">MyIoT - Graph All - Hours</title>
 @stop
 
@@ -27,12 +24,12 @@ $.ajaxSetup({
         <div id="chart1" class="text-center" ></div>
         <div class="panel-footer">
           <div class="row">
-            <div class="col-md-6">
+            <div class="col-sm-6">
               <h6 class="text-left" id="totalsampleschart1"  >Samples: {{count($samples)}} </h6>
               <h6 class="text-left">Render Time: <div class="loadtimechart1"style="display: inline-block;"></div> seconds </h6>
               <button type="button" class="btn btn-primary btn-xs" id="reloadchart1" >Reload graph data</button>
             </div>
-            <div class="col-md-6">
+            <div class="col-sm-6">
               <h6 class="text-left" id="lastsampletimechart1">Last sample time: {{$settings['tlast_sample']}} </h6>
               <h6  class="text-left" id="tempschart1">temp - min: {{$settings['tmin']}}, max: {{$settings['tmax']}}, now: {{$settings['temp_now']}}, - tSPhi: {{$settings['tSPhi']}}, tSPlo; {{$settings['tSPlo']}}</h6>
             </div>
@@ -49,14 +46,14 @@ $.ajaxSetup({
         <div id="chart2" class="text-center"></div>
         <div class="panel-footer">
           <div class="row">
-            <div class="col-md-6">
-              <h6 class="text-left" id="totalsampleschart2"  >Samples: {{count($samples)}}</h6>
+            <div class="col-sm-6">
+              <h6 class="text-left" id="totalsampleschart2"  >Samples: </h6>
               <h6 class="text-left">Render Time: <div class="loadtimechart2"style="display: inline-block;"></div> seconds </h6>
               <button type="button" class="btn btn-primary btn-xs" id="reloadchart2" >Reload graph data</button>
             </div>
-            <div class="col-md-6">
-              <h6 class="text-left" id="lastsampletimechart2">Last sample time: {{$settings['tlast_sample']}} </h6>
-              <h6  class="text-left" id="tempschart2">temp - min: {{$settings['tmin']}}, max: {{$settings['tmax']}}, now: {{$settings['temp_now']}}, - tSPhi: {{$settings['tSPhi']}}, tSPlo; {{$settings['tSPlo']}}</h6>
+            <div class="col-sm-6">
+              <h6 class="text-left" id="lastsampletimechart2">Last sample time:  </h6>
+              <h6  class="text-left" id="tempschart2">temp - min: , max: , now: , - tSPhi: , tSPlo; </h6>
             </div>
           </div>
         </div>
@@ -65,13 +62,9 @@ $.ajaxSetup({
   </div>
 </div>
 
-
 @if ( session()->has('message') )
 <h6 class="text-center">
   <span class="glyphicon glyphicon-refresh"></span> {{ session()->get('message') }} <span class="glyphicon glyphicon-refresh"></span>
 </h6>
 @endif
-
-
-
 @stop
